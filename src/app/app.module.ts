81c233262a3c1af7a6271977381109793f7266cc
a.didenko.sd@gmail.com <a.didenko.sd@gmail.com>
2019-09-02 23:11:23 +0300

    Create list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RootComponent } from './root.component';
+import { PagesListComponent } from "./pages/pages-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    RootComponent,
+    PagesListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/pages-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pages-list',
+    template: '<h2>Page list component</h2>'
+})
+export class PagesListComponent {
+
+}
